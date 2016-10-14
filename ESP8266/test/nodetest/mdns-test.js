0060a13692f19756d26c212cab4dccf0f763f784
Erland Lewin <erland@lewin.nu>
2016-10-14 12:20:45 +0200

    Continued work on mdns-test, adapt to new protocol with device IDs

--- ESP8266/test/nodetest/mdns-test.js
+++ ESP8266/test/nodetest/mdns-test.js
@@ -3,6 +3,11 @@ var mdns = require('multicast-dns')()
 
 var os = require('os');
 var ws = require('ws');
+const readline = require('readline');
+
+var WebSocketServer = require('ws').Server
+var devicesByConnection = {};
+var devicesByTypeID = {};
 
 // get my IP address
 var ifaces = os.networkInterfaces();
@@ -71,14 +76,97 @@ mdns.on('query', function(query)
 });
 
 // create websocket server
-var WebSocketServer = require('ws').Server
 
 wss = new WebSocketServer({ port: 80 });
 
 wss.on('connection', function connection(ws) {
-	ws.on('message', function incoming(message) {
+	device = { 'connection': ws };
+	devicesByConnection[ ws ] = device;
+
+	ws.on('message', function handleMessage(message) {
+		parseNetworkMessage( device, message );
 		console.log('received: %s', message);
 	    });
 
-	ws.send('something');
+	// ws.send('something');
     });
+
+// read from standard input
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+    });
+
+function parseDevice( data )
+{
+    var device = devicesByTypeID[ data ];
+    if( device )
+	rl.question( 'Enter data for ' + data + ": ", function( message ) { sendData( device, message ) } );
+    else
+    {
+	console.log( "No such device" );
+	rl.question('Send to what device (type-id):', parseDevice );
+    }
+
+    // ws.send( data );
+}
+
+function sendData( device, data )
+{
+    device[ 'connection' ].send( data );
+}
+
+function startsWith( string1, string2 )
+{
+    return string1.substring(0, string2.length) == string2
+}
+
+function parseNetworkMessage( device, message )
+{
+    var type = device[ 'type' ];
+
+    if( type )
+	console.log( type + "-" + device[ 'id' ] + ": '" + message + "'" );
+    else
+    {
+	if( startsWith( message, "ID:" ) )
+        {
+	    var restOfString;
+	    var charIndex;
+	    var typeID;
+
+	    restOfString = message.substring( 3 );
+	    charIndex = restOfString.indexOf( '-' );
+
+	    device[ 'type' ] = restOfString.substring( 0, charIndex );
+	    restOfString = restOfString.substring( charIndex + 1 );
+
+	    charIndex = restOfString.indexOf( ':' );
+	    
+	    // using trim to trim off newlines at the end
+	    if( charIndex == -1 )
+		device[ 'id' ] = restOfString.trim();
+	    else
+	    {
+		device[ 'id' ] = restOfString.substring( 0, charIndex );
+		device[ 'details' ] = restOfString.substring( charIndex + 1 ).trim();
+	    }
+
+	    typeID = device[ 'type' ] + "-" + device[ 'id' ];
+	    devicesByTypeID[ typeID ] = device;
+	    
+	    console.log( "Parsed ID " + typeID );
+	}
+	else
+	    console.log( "Strange, message '" + message + "' doesn't start with ID:" );
+    }
+}
+
+rl.question('Send to what device (type-id):', parseDevice );
+
+//(answer) => {
+	// TODO: Log the answer in a database
+//	console.log('Thank you for your valuable feedback:', answer);
+
+//	rl.close();
+//    });
